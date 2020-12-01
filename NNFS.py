c242c15c815de5aafc95d3090c0ef4af451b7749
Angel Mitkov <naturexe@gmail.com>
2020-12-01 19:07:58 +0200

    Working version!

--- NNFS.py
+++ NNFS.py
@@ -9,8 +9,8 @@ class Layer_Dense:
 
     # Layer initialization
     def __init__(self, n_inputs, n_neurons,
-        weight_regularizer_l1=0, weight_regularizer_l2=0,
-        bias_regularizer_l1=0, bias_regularizer_l2=0):
+                 weight_regularizer_l1=0, weight_regularizer_l2=0,
+                 bias_regularizer_l1=0, bias_regularizer_l2=0):
 
         # Initialize weights and biases
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
@@ -23,7 +23,6 @@ class Layer_Dense:
 
     # Forward pass
     def forward(self, inputs):
-
         # Remember input values
         self.inputs = inputs
         # Calculate output values from inputs, weights and biases
@@ -31,31 +30,31 @@ class Layer_Dense:
 
     # Backward pass
     def backward(self, dvalues):
-
         # Gradients on parameters
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
         # Gradients on regularization
         # L1 on weights
         if self.weight_regularizer_l1 > 0:
-           dL1 = np.ones_like(self.weights)
-           dL1[self.weights < 0] = -1
-           self.dweights += self.weight_regularizer_l1 * dL2
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
         # L2 on weights
         if self.weight_regularizer_l2 > 0:
-           self.dweights += 2 * self.weight_regularizer_l2 * \
-           self.weights
+            self.dweights += 2 * self.weight_regularizer_l2 * \
+            self.weights
         # L1 on biases
         if self.bias_regularizer_l1 > 0:
-           dL1 = np.ones_like(self.biases)
-           dL1[self.biases < 0] = -1
-           self.dbiases += self.bias_regularizer_l1 * dL1
-           # L2 on biases
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+        # L2 on biases
         if self.bias_regularizer_l2 > 0:
-           self.dbiases += 2 * self.bias_regularizer_l2 * \
-           self.biases
+            self.dbiases += 2 * self.bias_regularizer_l2 * \
+                    self.biases
            # Gradient on values
-           self.dinputs = np.dot(dvalues, self.weights.T)
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 
 # ReLU activation
